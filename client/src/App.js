b460b6502745ca16f956ddc9a9c3631f7b0b1647
Dilraj Singh <Diluppal2399@outlook.com>
2021-10-29 01:04:59 -0400

    cc

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Head from 'next/head'
+//material UI imports. Material UI is just a UI kit which have some predefined ematerials that we can use.
+import { Typography, AppBar } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//importing component
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
+
+//This where our UI styles for the app will go.
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+
+const App = () => {
+
+    {/*
+    *Now we will use our syles created above. 
+    */}
+    const classes = useStyles();
+
+    return (
+
+        <div className={classes.wrapper}>
+
+        {/*Header component*/}
+            <AppBar position="static" color="inherit" className={classes.appBar}>
+            <Typography varient="h2" align="center"> SkillBox Video Call</Typography>
+            </AppBar>
+        
+        {/* Video Player */}
+         <VideoPlayer />
+
+         {/* Options Menu. This includes -> Notiifications(When somebody calls you)*/}
+        <Options>
+        <Notifications />
+        </Options>
+      
+
+        </div>
+    );
 }
 
-export default App;
+export default App
